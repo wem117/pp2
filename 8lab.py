fe8759fe3301a0ef7d08e6fdc364289f0471c6df
wem117 <zmiras17r@gmail.com>
2025-04-05 13:15:36 +0500

    Add files via upload

---
+++ 8lab.py
@@ -0,0 +1,5 @@
+<script>start("/Applications/python/8lab.py/");</script>
+<script>onHasParentDirectory();</script>
+<script>addRow("1.py","1.py",0,2955,"2.9 kB",1743835499,"05.04.2025, 11:44:59");</script>
+<script>addRow("car.py","car.py",0,4946,"4.8 kB",1743840872,"05.04.2025, 13:14:32");</script>
+<script>addRow("snake.py","snake.py",0,2872,"2.8 kB",1743835617,"05.04.2025, 11:46:57");</script>
