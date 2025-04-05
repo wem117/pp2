5b49bde02f9a20cdbc4d29101911c52bef4f3aa5
wem117 <zmiras17r@gmail.com>
2025-04-05 13:42:14 +0500

    Add files via upload

---
+++ 9lab.py
@@ -0,0 +1,5 @@
+<script>start("/Applications/python/9lab.py/");</script>
+<script>onHasParentDirectory();</script>
+<script>addRow("car.py","car.py",0,5316,"5.2 kB",1743842238,"05.04.2025, 13:37:18");</script>
+<script>addRow("paint.py","paint.py",0,5139,"5.0 kB",1743838806,"05.04.2025, 12:40:06");</script>
+<script>addRow("snake.py","snake.py",0,4841,"4.7 kB",1743840028,"05.04.2025, 13:00:28");</script>
