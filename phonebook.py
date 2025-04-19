81b915ceb86519d0946cacc51fb1ec8523fa96e1
wem117 <zmiras17r@gmail.com>
2025-04-19 14:02:55 +0500

    Add files via upload

---
+++ phonebook.py
@@ -0,0 +1,4 @@
+<script>start("/Applications/python/phonebook.py/");</script>
+<script>onHasParentDirectory();</script>
+<script>addRow("10snake.py","10snake.py",0,5153,"5.0 kB",1745053353,"19.04.2025, 14:02:33");</script>
+<script>addRow("phb.py","phb.py",0,3143,"3.1 kB",1745040254,"19.04.2025, 10:24:14");</script>
